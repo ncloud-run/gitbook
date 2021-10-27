739577f982480cafe64d6236d2abce880423d020
nimoc <nimocc@126.com>
2021-10-28 01:22:35 +0800

    page.js

--- assets/page.js
+++ assets/page.js
@@ -1,3 +1,14 @@
 (function () {
-
+    document.querySelectorAll('.sidebar a').forEach(function (node) {
+        var path = location.pathname
+            .replace(/\/$/,'')
+            .replace(/\.html$/, '')
+            .replace(/\README$/, '')
+        var href = node.href.replace(location.origin, "")
+        console.log(path, href, node.innerHTML)
+        if (path == href) {
+            node.classList.add("sidebar-link--on")
+            node.focus()
+        }
+    })
 })();
