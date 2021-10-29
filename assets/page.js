70b3c9621de39d0ea47d166f5226e7989a391113
nimoc <nimocc@126.com>
2021-10-29 22:43:09 +0800

    mobile sidebar

--- assets/page.js
+++ assets/page.js
@@ -4,11 +4,15 @@
             .replace(/\/$/,'')
             .replace(/\.html$/, '')
             .replace(/\README$/, '')
-        var href = node.href.replace(location.origin, "").replace(/\?.*$/, '')
-        console.log(path, href, node.innerHTML)
+        var href = node.href.replace(location.origin, "")
+            .replace(/\?.*$/, '')
+            .replace(/\/$/,'')
+            .replace(/\.html$/, '')
+            .replace(/\README$/, '')
         if (path == href) {
             node.classList.add("sidebar-link--on")
             node.focus()
         }
+        console.log("made with https://github.com/2type/gitbook")
     })
 })();
