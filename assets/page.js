41e2701024567951d7c1c40983ae9440dbb9f441
nimoc <nimocc@126.com>
2021-12-28 23:29:45 +0800

    blob embed

--- assets/page.js
+++ assets/page.js
@@ -1,4 +1,69 @@
-(function () {
+// markdown-sidebar
+!(function () {
+    function markrunSidebar(settings) {
+        settings = settings || {}
+        settings.content = settings.content || document.body
+        var map = {}
+        var sidebar = document.createElement('ul')
+        var childNodes = settings.content.querySelectorAll('h2,h3')
+        var childNodesLength = childNodes.length
+        var currentSubHeaderId = null
+        childNodes.forEach(function(element){
+            var id = element.id
+            var text = element.innerText
+            var data = {
+                id: id,
+                text: text
+            }
+            switch (element.tagName) {
+                case 'H2':
+                    currentSubHeaderId = id
+                    map[id] = map[id] || data
+                    break
+                case 'H3':
+                    if (currentSubHeaderId !== null) {
+                        map[currentSubHeaderId].child = map[currentSubHeaderId].child ||[]
+                        map[currentSubHeaderId].child.push(data)
+                    }
+                    break
+                default:
+            }
+        })
+        for(var key in map) {
+            var item = map[key]
+            var li = document.createElement('li')
+            var link = document.createElement('a')
+            link.innerHTML = item.text
+            link.setAttribute('href', '#' + item.id)
+            link.setAttribute('class', 'markdown-sidebar-link')
+            li.appendChild(link)
+            sidebar.appendChild(li)
+            if (item.child) {
+                var littleUl = document.createElement('ul')
+                item.child.forEach(function (littleTitle) {
+                    var littleLi = document.createElement('li')
+                    var littleLink = document.createElement('a')
+                    littleLink.innerHTML = littleTitle.text
+                    littleLink.setAttribute('href', '#' + littleTitle.id)
+                    littleLink.setAttribute('class', 'markdown-sidebar-link')
+                    littleLi.appendChild(littleLink)
+                    littleUl.appendChild(littleLi)
+                })
+                li.appendChild(littleUl)
+            }
+        }
+        settings.element.appendChild(sidebar)
+        return map
+    }
+    if (typeof window !== 'undefined') {
+        window.markrunSidebar = markrunSidebar
+    }
+    if (typeof module !== 'undefined') {
+        module.exports = markrunSidebar
+    }
+})()
+
+;(function () {
     document.querySelectorAll('#nav a').forEach(function (node) {
         var path = location.pathname
             .replace(/\/$/,'')
@@ -51,3 +116,93 @@
         document.head.appendChild(style)
     }, 2000)
 })();
+
+
+
+function getBlobURL(href) {
+    var sourcePath = href.replace(location.origin, '').replace(location.pathname, "").replace("?blob","?").replace("?embed","?")
+    var dirPath = ""
+    if (PagePath.indexOf("/") == -1) {
+        dirPath = ""
+    } else {
+        dirPath = PagePath.replace(/\/[^/]*$/, "/")
+    }
+    var onlineHref = GithubRepoURL + "/blob/" + GithubBranch  + "/"+ dirPath + sourcePath
+    return onlineHref
+}
+function getRawURL(href) {
+    var sourcePath = href.replace(location.origin, '').replace(location.pathname, "").replace("?blob","?").replace("?embed","?")
+    var dirPath = ""
+    if (PagePath.indexOf("/") == -1) {
+        dirPath = ""
+    } else {
+        dirPath = PagePath.replace(/\/[^/]*$/, "/")
+    }
+    var onlineHref = "https://raw.githubusercontent.com/" + GithubRepoNwo + "/" +GithubBranch  + "/"+ dirPath + sourcePath
+    return onlineHref
+}
+var onload = function (){
+    // sidebarBaseContent
+    if (PageSidebarBasedOnContent) {
+        var navContent = document.querySelector("#nav .nav-content")
+        navContent.innerHTML = ""
+        var markrunSideData = markrunSidebar({
+            content: document.querySelector(".content .markdown-body"),
+            element: navContent
+        })
+    }
+    // ?blob ?embed
+    document.querySelectorAll('a').forEach(function (node) {
+        var blobReg = /\?blob/g
+        if (blobReg.test(node.href)) {
+            node.setAttribute('target', "_blank")
+            node.href = getBlobURL(node.href)
+            var githubIcon = document.createElement("span")
+            githubIcon.className = "fa fa-github"
+            githubIcon.style = "margin-right:.3em;"
+            node.prepend(githubIcon)
+        }
+        var embedReg = /\?embed/g
+        if (embedReg.test(node.href)) {
+            var blobURL = getBlobURL(node.href)
+            var rawURL = getRawURL(node.href)
+            var text = fetch(rawURL).then(function (res){
+                if (res.status == 200) {
+                    return res.text()
+                }
+                return new Promise(function (resolve){
+                    resolve(null)
+                })
+            }).then(function (source){
+                if (!source) {
+                    return
+                }
+                text = "// " + blobURL + "\n" + source
+                html = hljs.highlightAuto(text).value
+
+                var box = document.createElement('div')
+                box.style.position = "relative"
+                var pre = document.createElement('pre')
+                pre.className = "embed-code"
+                var code = document.createElement('code')
+
+                code.innerHTML = html
+                pre.appendChild(code)
+                var sourceLink = document.createElement("a")
+                sourceLink.innerText = node.innerText
+                sourceLink.href = blobURL
+                sourceLink.target="_blank"
+                var githubIcon = document.createElement("span")
+                githubIcon.className = "fa fa-github"
+                githubIcon.style = "margin-right:.3em;"
+                sourceLink.prepend(githubIcon)
+                box.appendChild(sourceLink)
+                box.appendChild(pre)
+
+                node.parentNode.replaceChild(box,node)
+
+            })
+        }
+    })
+}
+document.addEventListener("DOMContentLoaded", onload, false)
--- readme.md
+++ readme.md
@@ -6,7 +6,7 @@ permalink: /
 
 # 2type/gitbook
 
-> gitbook 在国内访问速度慢,而且经常打开白屏,所以我做了一个 jekyll 版 gitbook主题,可以自行部署静态资源在各大平台.
+> gitbook 在国内访问速度慢,而且可能打开崩溃完全不可用,所以我做了一个 jekyll 版 gitbook主题,可以自行部署静态资源在各大平台.
 
 示例项目 [https://be.nimo.run](https://be.nimo.run)
 
@@ -35,26 +35,65 @@ remote_theme: 2type/gitbook
     abc
 
 ### permalink
-在 markdown 顶部配置 permalink 可自定义 URL 
+在 markdown 文件顶部配置 permalink 可自定义 URL 
 ```
 ---
 permalink: /some/
 ---
 ```
 
+## 基于内容生成侧栏
+
+在 markdown 文件顶部配置 `sidebarBasedOnContent: true` 
+侧栏将不使用 `_includes/SUMMARY.html` 文件,而是自动基于页面内容生成侧栏
+
+```
+---
+sidebarBasedOnContent: true
+---
+```
+
+
+## 代码
+
+### blob
+
+给链接添加 `?blob` 后缀后会自动将链接转换为 github 地址
+
+例如: 
+```
+[_config.yml](./_config.yml?blob)
+```
+
+[_config.yml](./_config.yml?blob)
+
+### embed
+
+给链接添加 `?embed` 后缀将代码嵌入到页面
+
+例如: 
+```
+[_config.yml](./_config.yml?embed)
+```
+
+[_config.yml](./_config.yml?embed)
+
 ### 锚点
  
-> `## 章节1 <a id="hash_name"></a>` 在标题后面加带 id 的空 a 标签,可以实现自定义锚点   
+```
+## 章节1 <a id="hash_name"></a>
+```
+在标题后面加带 id 的空 a 标签,可以实现自定义锚点   
 
 ## 定义页面内容
 
-* 修改左侧导航 [./_includes/SUMMARY.html](./_includes/SUMMARY.html)
-* 修改头部导航 [./_includes/HEADER_LINKS.html](./_includes/HEADER_LINKS.html)
-* 修改底部 [./_includes/FOOTER.html](./_includes/FOOTER.html)
+* 修改左侧导航 [./_includes/SUMMARY.html](./_includes/SUMMARY.html?blob)
+* 修改头部导航 [./_includes/HEADER_LINKS.html](./_includes/HEADER_LINKS.html?blob)
+* 修改底部 [./_includes/FOOTER.html](./_includes/FOOTER.html?blob)
 
 ## 样式
 
-修改 [./assets/var.css](./assets/var.css) 文件
+修改 [./assets/var.css](./assets/var.css?blob) 文件
 
 ## 部署jekyll  <a id="publish_jeykyll"></a>
 
